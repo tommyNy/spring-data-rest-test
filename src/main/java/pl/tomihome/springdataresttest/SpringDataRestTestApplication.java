cfb1d26c2bc755914071153bb5f3090c796e19e2
Tomi <nytomasz@gmail.com>
2017-09-08 18:33:51 +0200

    Initial commit.

---
+++ src/main/java/pl/tomihome/springdataresttest/SpringDataRestTestApplication.java
@@ -0,0 +1,12 @@
+package pl.tomihome.springdataresttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestTestApplication.class, args);
+	}
+}
